edee9ae08b6261f828dbead4c3f6f40270b10389
tomklecha <tom.klecha.dev@gmail.com>
2020-05-25 17:38:23 +0100

    erased non used data class in LoginModel

--- features/login/src/main/java/com/can_apps/login/bresenter/LoginModel.kt
+++ features/login/src/main/java/com/can_apps/login/bresenter/LoginModel.kt
@@ -1,11 +1,8 @@
 package com.can_apps.login.bresenter
 
  internal sealed class LoginModel {
-    data class Name(val loginName: LoginModelName?) : LoginModel()
-    data class Password(val password: LoginModelPassword?) : LoginModel()
+
     data class Error(val message: LoginErrorModel) : LoginModel()
 }
 
 internal inline class LoginErrorModel(val value: String)
-internal inline class LoginModelName(val value: String)
-internal inline class LoginModelPassword(val value: String)
