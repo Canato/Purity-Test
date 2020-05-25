a97e6bfedb6e2daa426c6b2ce4a5aa5bc39bd181
tomklecha <tom.klecha.dev@gmail.com>
2020-05-25 17:35:45 +0100

    revert internal modificator

--- features/login/src/main/java/com/can_apps/login/bresenter/LoginModel.kt
+++ features/login/src/main/java/com/can_apps/login/bresenter/LoginModel.kt
@@ -1,11 +1,11 @@
 package com.can_apps.login.bresenter
 
- sealed class LoginModel {
+ internal sealed class LoginModel {
     data class Name(val loginName: LoginModelName?) : LoginModel()
     data class Password(val password: LoginModelPassword?) : LoginModel()
     data class Error(val message: LoginErrorModel) : LoginModel()
 }
 
- inline class LoginErrorModel(val value: String)
- inline class LoginModelName(val value: String)
- inline class LoginModelPassword(val value: String)
+internal inline class LoginErrorModel(val value: String)
+internal inline class LoginModelName(val value: String)
+internal inline class LoginModelPassword(val value: String)
