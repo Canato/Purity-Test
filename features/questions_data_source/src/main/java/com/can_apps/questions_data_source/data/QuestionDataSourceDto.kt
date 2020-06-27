a0ba1412aea4376d914be4c35fcd60e4b003eb47
Tomasz Klecha <35104050+tomklecha@users.noreply.github.com>
2020-06-27 11:36:40 +0100

    Questions Data Source Module (#30)
    * *first look on Questions Data Source Module
    * created dto

---
+++ features/questions_data_source/src/main/java/com/can_apps/questions_data_source/data/QuestionDataSourceDto.kt
@@ -0,0 +1,19 @@
+package com.can_apps.questions_data_source.data
+
+import com.google.gson.annotations.SerializedName
+
+internal class QuestionDataSourceDto(
+    @SerializedName("category_name")
+    var categoryName: String,
+
+    @SerializedName("questions")
+    var questions: Set<Question>
+)
+
+internal data class Question(
+    @SerializedName("id")
+    var id: Int,
+
+    @SerializedName("weight")
+    var weight: Int
+)
