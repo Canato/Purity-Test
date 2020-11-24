194ac4a996d8d68833516c44ec7720ead099e9d5
tomklecha <tom.klecha.dev@gmail.com>
2020-11-24 17:38:21 +0000

    updated UI test for navigation

--- features/home/src/androidTest/java/com/can_apps/home/HomeFragmentTest.kt
+++ features/home/src/androidTest/java/com/can_apps/home/HomeFragmentTest.kt
@@ -1,6 +1,8 @@
 package com.can_apps.home
 
+import androidx.fragment.app.testing.FragmentScenario
 import androidx.fragment.app.testing.launchFragmentInContainer
+import androidx.lifecycle.Lifecycle
 import androidx.navigation.Navigation
 import androidx.navigation.testing.TestNavHostController
 import androidx.test.core.app.ApplicationProvider
@@ -10,30 +12,61 @@ import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.can_apps.home.app.HomeFragment
 import junit.framework.Assert.assertEquals
+import org.junit.After
+import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
-class HomeFragmentTest {
+internal class HomeFragmentTest {
+
+    lateinit var scenario: FragmentScenario<HomeFragment>
+
+    @Before
+    fun setUp() {
+        scenario = launchFragmentInContainer<HomeFragment>()
+    }
+
+    @After
+    fun tearDown() {
+        scenario.moveToState(Lifecycle.State.DESTROYED)
+    }
 
     @Test
-    fun givenHomeScreen_whenOnQuestionButtonClicked_thenNavigateToQuestionsFragment() {
-        //Given
+    fun givenHomeScreen_whenQuestionButtonClicked_thenNavigateToQuestionsFragment() {
+        // Given
         val navController = TestNavHostController(
             ApplicationProvider.getApplicationContext()
         )
         navController.setGraph(R.navigation.nav_graph_home)
 
-        val titleScenario = launchFragmentInContainer<HomeFragment>()
-
-        titleScenario.onFragment { fragment ->
+        scenario.onFragment { fragment ->
             Navigation.setViewNavController(fragment.requireView(), navController)
         }
 
-        //When
+        // When
         onView(ViewMatchers.withId(R.id.questionButton)).perform(ViewActions.click())
 
-        //Then
+        // Then
         assertEquals(navController.currentDestination?.id, R.id.questionsFragment)
+    }
+
+    @Test
+    fun givenHomeScreen_whenLoginButtonClicked_thenNavigateToLoginFragment() {
+        // Given
+        val navController = TestNavHostController(
+            ApplicationProvider.getApplicationContext()
+        )
+        navController.setGraph(R.navigation.nav_graph_home)
+
+        scenario.onFragment { fragment ->
+            Navigation.setViewNavController(fragment.requireView(), navController)
+        }
+
+        // When
+        onView(ViewMatchers.withId(R.id.loginButton)).perform(ViewActions.click())
+
+        // Then
+        assertEquals(navController.currentDestination?.id, R.id.loginFragment)
     }
 }
